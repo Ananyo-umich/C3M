cd008096cb04a07a5f7a68843872c825ba336326
Ananyo Bhattacharya <ananyo@csrwks2021-0039.engin.umich.edu>
2022-10-19 17:43:24 -0400

    Thermochemical databases added

--- tests/test_thermochemistry1D.cpp
+++ tests/test_thermochemistry1D.cpp
@@ -59,34 +59,43 @@ double *flux_out = new double [gas->nSpecies()];
 //Domain
 int nsp = gas->nSpecies();
 int nSize = 100;
-int nTime = 1000000;
-int size = nsp +3;
-int iTemp = nsp;
-int iPress = nsp + 1;
-int iKzz = nsp + 2;
+int nTime = 10000;
+int iTemp = 0;
+int iPress =  1;
+int iKzz =  2;
+int iAlt = 3;
 
-VectorXd m_wdot(gas->nSpecies());
-VectorXd mole_frac(gas->nSpecies());
-MatrixXd Alldata(size, nSize);
+VectorXd m_wdot(nsp);
+VectorXd mole_frac(nsp);
+VectorXd Un(nsp);
+VectorXd Unext(nsp);
+VectorXd Uprev(nsp);
+VectorXd diff_flux(nsp);
+VectorXd dQ(nsp);
+double Keddy(nSize);
+MatrixXd AtmData(4, nSize);
 MatrixXd b(nsp, nSize);
 //The extra dimensions stand for temperature, pressure and flux
 
 //Initial Condition
-//The input variables like T, P, eddy diffusion, species can be included from a netCDF
+//The input variables like T, P, Height, eddy diffusion, species can be included from a netCDF
 //file
 for (int i = 0; i < nSize; i++) {
       Temp = 500.0 + 100.0*i;
       Kzz = 1E12 + 1E11*i;
-      Alldata(iTemp,i) = Temp; 
-      Alldata(iKzz, i) = Kzz;
+      AtmData(iAlt, i) = i;
+      AtmData(iTemp,i) = Temp; 
+      AtmData(iKzz, i) = Kzz;
       gas->getMoleFractions(&mole_frac[0]);
-      Alldata(H2O,i) = mole_frac[H2O];
+      b.col(i) = mole_frac;
     }
 
 
 //Chemical evolution
-float dt = 1e-4;
-  
+float dt = 1e-8;
+float dh = 0.1; //Km
+int iPrev, iNext;
+
 //Initiating Matrices for Time Evolution
 Eigen::SparseMatrix<double>  m_wjac;
 m_wjac.resize(nsp, nsp);
@@ -94,20 +103,41 @@ MatrixXd mat1(nsp, nsp);
 mat1 = MatrixXd::Identity(nsp, nsp);
 MatrixXd mat2(nsp, nsp);
 
+
+//Iterating over each time step
+//Iterating over each grid point
+//Iterating over each species
+
 for (int i = 0; i < nTime; i++) {
   for (int j = 1; j < nSize-1; j++) {
+//Setting T, P, X for each grid point
+    iPrev = j-1;
+    iNext = j+1;
+    Temp = AtmData(iTemp,j);
+    gas->setState_TP(Temp, 300.0*OneAtm);
+    mole_frac = b.col(j);
+    gas->setMoleFractions(&mole_frac[0]);
+    Keddy = AtmData(iKzz, j);
+//Solving the net production for each species
     gas_kin->getNetProductionRates(&m_wdot[0]);
     m_wjac = gas_kin->netProductionRates_ddX();
     mat2 = ((mat1/dt) - m_wjac);
     mat2 = mat2.inverse();
-    mat2 = mat2*m_wdot;
-    gas->getMoleFractions(&mole_frac[0]);
-    mole_frac = mole_frac + mat2;
-    gas->setMoleFractions(&mole_frac[0]);
-    b(H2O, j) = mole_frac[H2O];
-
-//Eddy diffusion and upwind diffusion scheme
-   
+    mat2 = mat2*m_wdot; 
+//Diffusion terms
+    Un = b.col(j);
+    Unext = b.col(iNext);
+    Uprev = b.col(iPrev);
+    diff_flux = Keddy*(Unext - (2*Un) + Uprev)/(dh*dh);
+//Integration for each species (Foward Euler simple case)
+    dQ = mat2;
+    b.col(j) = b.col(j) +  dQ;
+    std::cout << std::endl;
+    std::cout << "At time step t = ";
+    std::cout << i*dt << " (s) ";
+    std::cout << "mole fractions = ";
+    std::cout << b.col(j).transpose() << " ";
+    std::cout << std::endl;
   }
 
 
