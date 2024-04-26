f920f291d5c3aa6ed5440f30d0ed4ca4c9ac5890
stormy/cli <chengcli@umich.edu>
2024-04-26 10:14:13 -0400

    wip

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -16,22 +16,6 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
   set(KNOWN_CXX_COMPILER TRUE)
 endif()
 
-if(CMAKE_C_COMPILER_ID MATCHES "GNU")
-  if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(C3M_C_FLAGS
-        "-g3"
-        CACHE INTERNAL "C3M C compiler flags")
-  elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
-    set(C3M_C_FLAGS
-        "-O3"
-        CACHE INTERNAL "C3M C compiler flags")
-  else()
-    message(FATAL_ERROR "Unknown build type: ${CMAKE_BUILD_TYPE}")
-  endif()
-
-  set(KNOWN_C_COMPILER TRUE)
-endif()
-
 # Setup for Clang compiler:
 #
 if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
@@ -48,22 +32,6 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   endif()
 
   set(KNOWN_CXX_COMPILER TRUE)
-endif()
-
-if(CMAKE_C_COMPILER_ID MATCHES "Clang")
-  if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(C3M_C_FLAGS
-        "-g3"
-        CACHE INTERNAL "C3M C compiler flags")
-  elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
-    set(C3M_C_FLAGS
-        "-O3"
-        CACHE INTERNAL "C3M C compiler flags")
-  else()
-    message(FATAL_ERROR "Unknown build type: ${CMAKE_BUILD_TYPE}")
-  endif()
-
-  set(KNOWN_C_COMPILER TRUE)
 endif()
 
 # Setup for ICC compiler:
@@ -84,26 +52,6 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
   set(KNOWN_CXX_COMPILER TRUE)
 endif()
 
-if(CMAKE_C_COMPILER_ID MATCHES "Intel")
-  if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(C3M_C_FLAGS
-        "-g3"
-        CACHE INTERNAL "C3M C compiler flags")
-  elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
-    set(C3M_C_FLAGS
-        "-O3"
-        CACHE INTERNAL "C3M C compiler flags")
-  else()
-    message(FATAL_ERROR "Unknown build type: ${CMAKE_BUILD_TYPE}")
-  endif()
-
-  set(KNOWN_C_COMPILER TRUE)
-endif()
-
 if(NOT KNOWN_CXX_COMPILER)
   message(FATAL_ERROR "\nUnknown C++ compiler!\n")
-endif()
-
-if(NOT KNOWN_C_COMPILER)
-  message(FATAL_ERROR "\nUnknown C compiler!\n")
 endif()
