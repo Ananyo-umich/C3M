09a0c8ce0ed41f35b9e301196de5101d9c90babe
stormy/cli <chengcli@umich.edu>
2024-04-26 14:47:16 -0400

    wip

---
+++ cmake/gtest.cmake
@@ -0,0 +1,11 @@
+include(FetchContent)
+
+set(FETCHCONTENT_QUIET TRUE)
+
+FetchContent_Declare(
+  gtest
+  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+  URL https://github.com/google/googletest/archive/refs/tags/v1.13.0.tar.gz)
+
+set(INSTALL_GTEST OFF)
+FetchContent_MakeAvailable(gtest)
---
+++ tests/PhotolysisBox.cpp
@@ -0,0 +1,63 @@
+// external
+#include <gtest/gtest.h>
+
+// application
+#include <application/application.hpp>
+
+// Solution class describes a phase consists of a mixture of chemical species
+#include <cantera/base/Solution.h>
+
+// ThermoPhase object stores the thermodynamic state
+#include <cantera/thermo.h>
+
+// Kinetics object stores the chemical kinetics information
+#include <cantera/kinetics.h>
+#include <cantera/kinetics/Reaction.h>
+
+TEST(ZeroDim, OxygenPhotolysisBox) {
+  // Reading the chemical kinetics network
+  auto sol = Cantera::newSolution("photolysis_box.yaml");
+
+  // Initial condition for mole fraction
+  sol->thermo()->setState_TPX(250., 0.1 * Cantera::OneAtm, "O2:0.21, N2:0.78");
+
+  // Calculating photochemical reaction rate
+  auto stellar_input_file = app->FindResource("stellar/sun.ir");
+  auto stellar_input =
+      ReadStellarRadiationInput(stellar_input_file, 1., 1.);
+  std::cout << "Radiation Input Complete!" << std::endl;
+
+  // Updating the actinic flux within yaml file [All in SI units]
+  sol->kinetics()->setWavelength(stellar_input.row(0).data(),
+                         stellar_input.row(0).size());
+  sol->kinetics()->updateActinicFlux(stellar_input.row(1).data());
+
+  // Reactor
+  Cantera::Reactor reactor(sol);
+  reactor.initialize();
+
+  // Reactor Network
+  Cantera::ReactorNet network;
+  network.addReactor(reactor);
+  network.initialize();
+
+  double time_step = 1.e-5;
+  double max_time = 1.e-2;
+
+  double time = 0.;
+  while (network.time() < max_time) {
+    time = network.time() + time_step;
+    network.advance(time);
+  }
+
+}
+
+int main(int argc, char **argv) {
+  Application::Start(argc, argv);
+  
+  int result = testing::InitGoogleTest(&argc, argv);
+
+  Application::Destroy();
+
+  return result;
+}
