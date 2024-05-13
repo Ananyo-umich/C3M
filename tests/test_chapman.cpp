5dc58cc9fa3f913ca1acf8519ebc6fdd1cf2083d
Ananyo Bhattacharya <ananyo@csrwks2022-0092.engin.umich.edu>
2024-05-13 14:26:48 -0400

    Chapman test wip

--- tests/test_chapman.cpp
+++ tests/test_chapman.cpp
@@ -30,17 +30,35 @@ TEST(Input, ChapmanCycle) {
   auto sol = Cantera::newSolution("chapman_box.yaml");
   YAML::Node config  = YAML::LoadFile("chapman_box.yaml");
 // Reading the temperature, and pressure for box input file
-
-
+// Find the node corresponding to "press"
+  YAML::Node pressNode = config["problem"][0]["pres"];
+  double Pres = atof(pressNode.as<std::string>().c_str());
+  if (pressNode) {
+            std::cout << "Found press: " << pressNode.as<std::string>() << std::endl;
+        } else {
+            std::cout << "press not found." << std::endl;
+        }
+  ASSERT_EQ(Pres, 2.5E-3);
+// Find the node corresponding to "temp"
+  YAML::Node tempNode = config["problem"][1]["temp"];
+  double Temp = atof(tempNode.as<std::string>().c_str());       
+  if (tempNode) {
+            std::cout << "Found temp: " << tempNode.as<std::string>() << std::endl;
+        } else {
+            std::cout << "temp not found." << std::endl;
+        }
+  ASSERT_EQ(Temp, 251);
 // Reading the time scale for integration
-
+  
 
 // Test 1: Earth's Ozone chemistry at 30 km
-
+  P1 = 2.5E-3
+  T1 = 251
 
 
 // Test 2: Earth's Ozone chemistry at 40 km
-
+  P2 = 2
+  T2 = 2 
   
 }
 
